b8e8574d9919de579adb9ff2a6a749c64ab10549
ChandrakantPatil3116 <chandrakant31716@gmail.com>
2026-07-25 06:11:49 +0530

    Initial Spring Boot Setup

---
+++ backend/src/main/java/com/campusnote/CampusnoteApplication.java
@@ -0,0 +1,13 @@
+package com.campusnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusnoteApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/campusnote/CampusnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

094c243088ec5e49c6cb79d134352186d8206017
ChandrakantPatil3116 <chandrakant31716@gmail.com>
2026-08-01 22:41:07 +0530

    Added repository inside UserServiceImpl.java

--- backend/src/main/java/com/campusnote/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/campusnote/service/impl/UserServiceImpl.java
@@ -3,11 +3,18 @@ package com.campusnote.service.impl;
 import com.campusnote.dto.LoginRequest;
 import com.campusnote.dto.RegisterRequest;
 import com.campusnote.dto.UserResponse;
+import com.campusnote.repository.UserRepository;
 import com.campusnote.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserResponse getUserById(Integer userId) {

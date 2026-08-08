18cda5542a51f3278673adb4aa6f3b9d09367b4e
ChandrakantPatil3116 <chandrakant31716@gmail.com>
2026-08-09 00:03:07 +0530

    modified UserService.java file

--- backend/src/main/java/com/campusnote/service/UserService.java
+++ backend/src/main/java/com/campusnote/service/UserService.java
@@ -1,6 +1,7 @@
 package com.campusnote.service;
 
 import com.campusnote.dto.LoginRequest;
+import com.campusnote.dto.LoginResponse;
 import com.campusnote.dto.RegisterRequest;
 import com.campusnote.dto.UserResponse;
 
@@ -8,7 +9,7 @@ public interface UserService {
 
     UserResponse register(RegisterRequest request);
 
-    UserResponse login(LoginRequest request);
+    LoginResponse login(LoginRequest request);
 
     UserResponse getUserById(Integer userId);
 }

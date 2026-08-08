0437f45c13bab2fca075dc1ac40d4dfc01160d8f
ChandrakantPatil3116 <chandrakant31716@gmail.com>
2026-08-09 00:58:26 +0530

    Modified SubjectRepository.java

--- backend/src/main/java/com/campusnote/repository/SubjectRepository.java
+++ backend/src/main/java/com/campusnote/repository/SubjectRepository.java
@@ -1,5 +1,8 @@
 package com.campusnote.repository;
 
-public class SubjectRepository {
-    
+import com.campusnote.entity.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+
 }

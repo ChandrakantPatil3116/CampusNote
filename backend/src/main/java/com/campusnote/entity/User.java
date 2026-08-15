86de4c23d83a147697bb2621176896b3648112e3
ChandrakantPatil3116 <chandrakant31716@gmail.com>
2026-08-15 16:24:45 +0530

    Fixed errors in User.java file

--- backend/src/main/java/com/campusnote/entity/User.java
+++ backend/src/main/java/com/campusnote/entity/User.java
@@ -1,8 +1,16 @@
 package com.campusnote.entity;
-import com.campusnote.entity.User;
-import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
-import jakarta.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")

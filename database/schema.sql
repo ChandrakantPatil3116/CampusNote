d80fd649896a9d7a0b5ff98747e7a1c7551fb27d
ChandrakantPatil3116 <chandrakant31716@gmail.com>
2026-09-04 01:45:16 +0530

    updated scheme.sql

--- database/schema.sql
+++ database/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role ENUM('student', 'admin') DEFAULT 'student',
+    role ENUM('STUDENT', 'ADMIN') DEFAULT 'STUDENT',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
